be38865cc38119adab500bacb4cfb4dcd21a1703
Jack Driscoll <jdriscoll98@ufl.edu>
2020-06-26 18:31:40 -0400

    launcher

--- ball.js
+++ ball.js
@@ -9,61 +9,77 @@ function Ball(tile) {
         this.y += this.velocity[1] * deltaTime;
         this.checkForCollision();
         if (this.velocity[0] < 0) {
-            this.velocity[0] += this.friction * deltaTime;
-            this.velocity[0] = Math.min(0, this.velocity[0]);
+            this.velocity[0] += -this.velocity[0] * this.friction * deltaTime;
+            if (this.velocity[0] > -0.01) {
+                this.velocity[0] = 0;
+            }
         }
         else if (this.velocity[0] > 0) {
-            this.velocity[0] -= this.friction * deltaTime;
-            this.velocity[0] = Math.max(0, this.velocity[0]);
-
+            this.velocity[0] -= this.velocity[0] * this.friction * deltaTime;
+            if (this.velocity[0] < 0.01) {
+                this.velocity[0] = 0;
+            }
         }
 
         if (this.velocity[1] < 0) {
-            this.velocity[1] += this.friction * deltaTime;
-            this.velocity[1] = Math.min(0, this.velocity[1]);
+            this.velocity[1] += -this.velocity[1] * this.friction * deltaTime;
+            if (this.velocity[1] > -0.01) {
+                this.velocity[1] = 0;
+            }
         }
         else if (this.velocity[1] > 0) {
-            this.velocity[1] -= this.friction * deltaTime;
-            this.velocity[1] = Math.max(0, this.velocity[1]);
+            this.velocity[1] -= this.velocity[1] * this.friction * deltaTime;
+            if (this.velocity[1] < 0.01) {
+                this.velocity[1] = 0;
+            }
 
         }
     }
     this.checkForCollision = function () {
-        currentY = Math.floor((this.y - 20) / 40);
-        currentX = Math.floor((this.x - 20) / 40);
+        currentY = Math.floor((this.y) / 40);
+        currentX = Math.floor((this.x) / 40);
 
         currentX_Left = Math.floor((this.x - 20) / 40);
-        currentX_Right = Math.floor((this.x) / 40);
+        currentX_Right = Math.floor((this.x + 20) / 40);
         currentY_Top = Math.floor((this.y - 20) / 40);
         currentY_Bottom = Math.floor((this.y + 20) / 40);
 
 
         // Bottom
-        if (game.tiles[currentX][currentY_Top + 1].type == 1) {
+        if (game.tiles[currentX][currentY_Bottom].type == 1) {
             this.velocity[1] = -this.velocity[1];
-            this.y -= 20;
+            this.y -= 5;
+
             return;
         }
         // Top
-        if (game.tiles[currentX][currentY_Bottom - 1].type == 1) {
+        if (game.tiles[currentX][currentY_Top].type == 1) {
             this.velocity[1] = -this.velocity[1];
-            this.y += 20;
+            this.y += 5;
+            // this.velocity = [0, 0];
+
             return;
         }
         // right
-        if (game.tiles[currentX_Left + 1][currentY].type == 1) {
+        if (game.tiles[currentX_Right][currentY].type == 1) {
             this.velocity[0] = -this.velocity[0];
-            this.x -= 20;
+            this.x -= 5;
+            // this.velocity = [0, 0];
+
             return;
         }
         // left
-        if (game.tiles[currentX_Right - 1][currentY].type == 1) {
+        if (game.tiles[currentX_Left][currentY].type == 1) {
             this.velocity[0] = -this.velocity[0];
-            this.x += 20;
+            this.x += 5;
+            // this.velocity = [0, 0];
+
             return;
         }
     }
     this.draw = function () {
         circle(this.x, this.y, 30);
+        text(this.load_velocity[0], this.x + 20, this.y)
+        text(this.load_velocity[1], this.x + 20, this.y + 20)
     }
 }
--- sketch.js
+++ sketch.js
@@ -15,12 +15,23 @@ function draw() {
             var x = Math.floor((mouseX - 5) / 40);
             var y = Math.floor((mouseY - 10) / 40);
             line(mouseX, mouseY, game.ball.x, game.ball.y);
-            distanceX = (mouseX - game.ball.x) / 100;
-            distanceY = (mouseY - game.ball.y) / 100;
+            distanceX = (mouseX - game.ball.x) / 300;
+            distanceY = (mouseY - game.ball.y) / 300;
             text(distanceX, mouseX, mouseY);
             text(distanceY, mouseX + 20, mouseY + 20);
-            game.ball.load_velocity[0] = -distanceX;
-            game.ball.load_velocity[1] = -distanceY;
+            if (distanceX > 0) {
+                game.ball.load_velocity[0] = Math.max(-distanceX, -1);
+            }
+            else {
+                game.ball.load_velocity[0] = Math.min(-distanceX, 1);
+            }
+            if (distanceY > 0) {
+                game.ball.load_velocity[1] = Math.max(-distanceY, -1);
+            }
+            else {
+                game.ball.load_velocity[1] = Math.min(-distanceY, 1);
+
+            }
         }
     }
     else {
